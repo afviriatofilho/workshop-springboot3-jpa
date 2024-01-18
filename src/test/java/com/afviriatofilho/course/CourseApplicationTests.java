346016c145ecfa10df197c55c68691b94af7f242
Alexandre Filho <afviriatofilho@gmail.com>
2024-01-18 15:02:04 -0300

    Project created

---
+++ src/test/java/com/afviriatofilho/course/CourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afviriatofilho.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
